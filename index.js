d15ebbd2549d6ef38c89cc23f39ee28411d4eaf1
unknown <qrockrocks@gmail.com>
2025-02-26 11:13:03 -0600

    Added a new monster fight to get riddler quiz

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const b4 = document.getElementById("button4")
 const img1 = document.getElementById("image1")
 let havesword = false
 let defeatedmonster = false
+let defeatedbeast = false
 let answeredquiz = false
 
 function restart(){
@@ -32,9 +33,17 @@ function restart(){
     b2.removeEventListener('click', monsterroom)
     b3.removeEventListener('click', monsterroom)
     b4.removeEventListener('click', monsterroom)
+    b1.removeEventListener('click', beastroom)
+    b2.removeEventListener('click', beastroom)
+    b3.removeEventListener('click', beastroom)
+    b4.removeEventListener('click', beastroom)
     b1.removeEventListener('click', quizroom)
     b2.removeEventListener('click', quizroom)
     b3.removeEventListener('click', quizroom)
+    b4.removeEventListener('click', leftchoice)
+    b1.removeEventListener('click', leftchoice)
+    b2.removeEventListener('click', leftchoice)
+    b3.removeEventListener('click', leftchoice)
     b4.removeEventListener('click', quizroom)
     b1.removeEventListener('click', swordtime)
     b2.removeEventListener('click', swordtime)
@@ -44,6 +53,10 @@ function restart(){
     b2.removeEventListener('click', monsterfight)
     b3.removeEventListener('click', monsterfight)
     b4.removeEventListener('click', monsterfight)
+    b1.removeEventListener('click', beastfight)
+    b2.removeEventListener('click', beastfight)
+    b3.removeEventListener('click', beastfight)
+    b4.removeEventListener('click', beastfight)
     b1.removeEventListener('click', takequiz)
     b2.removeEventListener('click', takequiz)
     b3.removeEventListener('click', takequiz)
@@ -114,7 +127,7 @@ function swordroom(){
         console.log("You are in the sword room")
         restart()
         b1.addEventListener('click', swordtime)
-        b2.addEventListener('click', die)
+        b2.addEventListener('click', leftchoice)
         b3.addEventListener('click', shart)
         b4.addEventListener('click', swordroom)
     }
@@ -128,7 +141,7 @@ function swordroom(){
         console.log("You returned the sword room")
         restart()
         b1.addEventListener('click', swordroom)
-        b2.addEventListener('click', die)
+        b2.addEventListener('click', leftchoice)
         b3.addEventListener('click', shart)
         b4.addEventListener('click', swordroom)
     }
@@ -208,6 +221,84 @@ function quizroom(){
     event.preventDefault()
 }
 
+function beastroom(){
+    if (defeatedbeast == false){
+        img1.src = "Beast Room.png"
+        text1.innerHTML = "You encountered a powerful monster"
+        button1.innerHTML = ""
+        button2.innerHTML = "Try to fight"
+        button3.innerHTML = "Go Back"
+        button4.innerHTML = ""
+        console.log("You are in the beast room")
+        restart()
+        if (havesword == false){
+            console.log(havesword)
+            b1.addEventListener('click', beastroom)
+            b2.addEventListener('click', die)
+            b3.addEventListener('click', leftchoice)
+            b4.addEventListener('click', beastroom)
+        }
+        else{
+            b1.addEventListener('click', beastroom)
+            b2.addEventListener('click', beastfight)
+            b3.addEventListener('click', leftchoice)
+            b4.addEventListener('click', monsterroom) 
+        }
+    }
+    else{
+        img1.src = "Re Beast Room.png"
+        text1.innerHTML = "There is nothing here."
+        button1.innerHTML = ""
+        button2.innerHTML = "Go Forward"
+        button3.innerHTML = "Go Back"
+        button4.innerHTML = ""
+        console.log("You returned the beast room")
+        restart()
+        b1.addEventListener('click', beastroom)
+        b2.addEventListener('click', die)
+        b3.addEventListener('click', leftchoice)
+        b4.addEventListener('click', beastroom)
+    }
+    event.preventDefault()
+}
+
+function beastfight(){
+    defeatedbeast=true
+    img1.src = "Beast Win.png"
+    text1.innerHTML = "You defeated the monster"
+    console.log("You have defeated the beast.")
+    button1.innerHTML = "Stay"
+    button2.innerHTML = "Go Forward"
+    button3.innerHTML = "Go Back"
+    button4.innerHTML = ""
+    restart()
+    b1.addEventListener('click', beastroom)
+    b2.addEventListener('click', die)
+    b3.addEventListener('click', leftchoice)
+    b4.addEventListener('click', beastfight)
+    event.preventDefault()
+}
+
+function ganonroom(){
+    console.log("Penis")
+}
+
+function leftchoice(){
+    img1.src = "Leftward Choice.png"
+    text1.innerHTML = "You reached a split path"
+    console.log("You entered the leftward split path room")
+    restart()
+    button1.innerHTML = "Right"
+    button2.innerHTML = ""
+    button3.innerHTML = "Go Back"
+    button4.innerHTML = "Left"
+    b1.addEventListener('click', beastroom)
+    b2.addEventListener('click', leftchoice)
+    b3.addEventListener('click', swordroom)
+    b4.addEventListener('click', ganonroom)
+    event.preventDefault()
+}
+
 function takequiz(){
     let a1 = prompt("What is the national pokedex number of Clampearl?")
     let a2 = prompt("Who is the final character in the friends intro?")
@@ -243,7 +334,7 @@ function swordtime(){
     button4.innerHTML = ""
     restart()
     b1.addEventListener('click', swordroom)
-    b2.addEventListener('click', die)
+    b2.addEventListener('click', leftchoice)
     b3.addEventListener('click', shart)
     b4.addEventListener('click', swordtime)
     event.preventDefault()
@@ -278,6 +369,7 @@ function start(){
     havesword = false
     defeatedmonster = false
     answeredquiz = false
+    defeatedbeast = false
     restart()
     console.log("You are in the first room")
     b1.addEventListener('click', lavaroom)
